b3eaa4196eeb74605d6ca42607307f1dc84db40a
Christian Simăn-Cherecheș <49637374+callmechristian@users.noreply.github.com>
2023-02-14 13:00:54 +0100

    visualisation

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
-#include "Point.h"
-#include "Line.h"
-#include "Interpolation.h"
+#include "Point.hpp"
+#include "Line.hpp"
+#include "Interpolation.hpp"
+#include <cstdlib> // System commands
 
-int main() {
+int main(int argc, char *argv[]) {
     int num_points = 5;
     std::vector<Point<double>> points;
+
+    // Check if the number of points was specified in the command-line
+    if (argc > 1) {
+        num_points = std::stoi(argv[1]);
+    }
 
     // Generate 5 random points
     for (int i = 0; i < num_points; i++) {
@@ -16,13 +22,31 @@ int main() {
     Interpolation<double> interp(points);
 
     // Interpolate the points
-    interp.linear(2);
+    interp.linear(5);
 
     // Print the interpolated points between each input point
     interp.printPretty();
 
-    interp.dumpToFile("interpolation.dat");
-    interp.dumpInputsToFile("input.dat");
+    // Handle filer errors
+    try {
+        interp.dumpToFile("interpolation.dat");
+        interp.dumpInputsToFile("input.dat");
+    } catch (std::exception e) {
+        std::cerr << "Files failed to write with error: " << e.what() << std::endl;
+    }
+
+    // Handle system errors from python sub-launch
+    try {
+        std::string command = "python plot_interpolation.py out/input.dat out/interpolation.dat";
+        int result = std::system(command.c_str());
+        if (result == 0) {
+            std::cout << "Script executed successfully." << std::endl;
+        } else {
+            std::cout << "Script execution failed." << std::endl;
+        }
+    } catch (std::exception e) {
+        std::cerr << "Interpolation visualization failed with error: " << e.what() << std::endl;
+    }
 
     return 0;
 }
