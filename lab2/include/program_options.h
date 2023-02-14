a21a68380192190540a7b66fb3259566a8259d80
Christian Simăn-Cherecheș <49637374+callmechristian@users.noreply.github.com>
2023-02-14 15:21:01 +0100

    better cmd line options

---
+++ lab2/include/program_options.h
@@ -0,0 +1,27 @@
+// Inspired from https://medium.com/@mostsignificant/3-ways-to-parse-command-line-arguments-in-c-quick-do-it-yourself-or-comprehensive-36913284460f
+
+#ifndef THAT_THIS_PROGRAM_OPTIONS_HEADER_FILE_IS_ALREADY_INCLUDED
+#define THAT_THIS_PROGRAM_OPTIONS_HEADER_FILE_IS_ALREADY_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace program_options {
+
+    void parse(int argc, char* argv[]);
+
+    const std::vector<std::string> input_files();
+    std::string get_option(const std::vector<std::string>& args, const std::string& option_name);
+    bool has_option(const std::vector<std::string>& args, const std::string& option_name);
+    bool has_files(const std::vector<std::string>& args);
+    bool show_ends();
+    bool show_line_numbers();
+
+    // TODO: Overwrite numpoints if input file specified
+    int num_points = 5; // generate 5 random points by default
+    // TODO: print how many samples were generated
+    int samples = 3; // interpolate between them in 3 samples by default
+
+};  // namespace program_options
+
+#endif  // THAT_THIS_PROGRAM_OPTIONS_HEADER_FILE_IS_ALREADY_INCLUDED
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,33 +1,71 @@
 #include <iostream>
 #include <vector>
-#include "Point.hpp"
-#include "Line.hpp"
-#include "Interpolation.hpp"
-#include <cstdlib> // System commands
+#include "include/Point.hpp"
+#include "include/Line.hpp"
+#include "include/Interpolation.hpp"
+// System commands
+#include <cstdlib>
+// Command line arguments parsing
+#include "include/program_options.h"
+#include "include/program_options.cpp"
 
 int main(int argc, char *argv[]) {
-    int num_points = 5;
+
+    // // Generate 5 random numbers if unspecified
+    // program_options::num_points = 5;
+    // // Set samples to 3 if unspecified
+    // program_options::samples = 3;
+
+    // Vector to store the points
     std::vector<Point<double>> points;
 
-    // Check if the number of points was specified in the command-line
-    if (argc > 1) {
-        num_points = std::stoi(argv[1]);
+    // Parse arg list
+    try {
+        program_options::parse(argc, argv);
+    } catch (const std::exception &x) {
+        std::cerr << x.what() << '\n';
+        std::cerr << "usage: [-n] [-s] <input_file> ...\n";
+        return EXIT_FAILURE;
     }
 
-    // Generate 5 random points
-    for (int i = 0; i < num_points; i++) {
-    points.push_back(Point<double>(rand() % 10, rand() % 10));
+    // Parse input files from arg list
+    for (const auto &file_name : program_options::input_files()) {
+        std::ifstream input_file(file_name.data(), std::ios::in);
+        if (!input_file.is_open()) {
+            std::cerr << "Could not open input file '" << file_name << "'!\n";
+            return EXIT_FAILURE;
+        }
+
+        std::string line;
+        int         line_count = 1;
+        while (std::getline(input_file, line)) {
+            if (program_options::show_line_numbers()) {
+                std::cout << std::setw(6) << std::setfill(' ') << line_count++ << "  ";
+            }
+
+            if (program_options::show_ends()) {
+                std::cout << '$';
+            }
+
+            std::cout << '\n';
+        }
     }
 
+    // Generate n random points
+    for (int i = 0; i < program_options::num_points; i++) {
+        points.push_back(Point<double>(rand() % 10, rand() % 10));
+    }
+
+    // Create interpolation object
     Interpolation<double> interp(points);
 
-    // Interpolate the points
-    interp.linear(5);
+    // Interpolate the points with s samples
+    interp.linear(program_options::samples);
 
     // Print the interpolated points between each input point
     interp.printPretty();
 
-    // Handle filer errors
+    // Handle file errors
     try {
         interp.dumpToFile("interpolation.dat");
         interp.dumpInputsToFile("input.dat");
